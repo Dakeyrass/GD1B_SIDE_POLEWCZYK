e8ac7ab4b62de0b8ae6c828323163bc185f2a362
Dakeyrass <pwkmarion@gmail.com>
2024-01-23 22:31:35 +0100

    Base

---
+++ Assets/Scripts/Controles.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controles : MonoBehaviour
+{   
+    [SerializeField] private float speed;
+    [SerializeField] private float jump_force;
+    private Rigidbody2D body;
+    private bool canJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        //courir
+        body.velocity = new Vector2(horizontal*speed,body.velocity.y);
+        //saut
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true){
+            body.velocity = new Vector2 (body.velocity.x,jump_force);
+            canJump=false;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Ground"){
+            canJump=true;
+        }
+        if (collision.gameObject.tag == "Wall"){
+            canJump=true;
+            body.velocity = new Vector2(body.velocity.x,-2);
+        }
+        
+    }
+}
