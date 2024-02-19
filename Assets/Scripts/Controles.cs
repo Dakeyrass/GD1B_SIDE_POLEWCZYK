e8e12aaf99cc4721dde71f9a74c895d806ed82b2
Dakeyrass <pwkmarion@gmail.com>
2024-02-19 23:48:35 +0100

    power up et animations
    pitié sauvez moi

--- Assets/Scripts/Controles.cs
+++ Assets/Scripts/Controles.cs
@@ -7,6 +7,7 @@ public class Controles : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jump_force;
     private Rigidbody2D body;
+    private Animator anim;
     private bool Grounded;
     private bool canDJump;
     public bool powerup=false;
@@ -16,6 +17,7 @@ public class Controles : MonoBehaviour
     void Start()
     {
         body = GetComponent <Rigidbody2D>();
+        anim = GetComponent <Animator>();
     }
 
     // Update is called once per frame
@@ -32,10 +34,14 @@ public class Controles : MonoBehaviour
         }
         //flip
         if (horizontal>0){
-            transform.localScale = new Vector3(0.5f,0.5f);
+            transform.localScale = new Vector3(1f,1f);
         } else if (horizontal<0){
-            transform.localScale = new Vector3(-0.5f,0.5f);
+            transform.localScale = new Vector3(-1f,1f);
         }
+
+        //animation
+        anim.SetBool("run", horizontal !=0);
+        anim.SetBool("grounded", Grounded);
     }
     
     private void OnCollisionEnter2D(Collision2D collision){
@@ -57,6 +63,12 @@ public class Controles : MonoBehaviour
         if (powerup==true){
             body.velocity = new Vector2 (body.velocity.x,jump_force);
             canDJump=false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("Power_up")){
+            powerup=true;  
         }
     }
 }
--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
+public BoxCollider2D bc2d;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+private void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.CompareTag("Character"))
+    {
+        Debug.Log("oui");
+        Destroy(bc2d.gameObject);
+    }
+}
     // Update is called once per frame
     void Update()
     {
         
     }
-    OnTriggerEnter2D(Collider2D other){
-        if (other.ComparTag("Player")){
-            Controles control = player.GetComponent<Controles>();
-            control.powerup = true; 
-
-            Destroy(gameObject);  
-        }
-    }
+    
     
 }
 
